18064e0e231c764f7df497e324adce93e9e7de90
ot561 <ot561@york.ac.uk>
2016-04-16 14:09:59 +0100

    Added files via upload

--- blockavg.py
+++ blockavg.py
@@ -1,9 +1,16 @@
+from io import StringIO
 import numpy as np
+import os
 
-
+os.chdir(r'C:\Users\Oliver\Desktop')
+print os.getcwd()
 data_array = np.genfromtxt('data.txt', delimiter=',')
+#data_array = np.loadtxt('data.txt', delimiter=',', usecols=[0])
 
-x=2
-np.mean(data_array.reshape(-1, x), 1)
+#data_array = open("test.txt", "r")
 
-np.savetxt('avgdata.dat', dat, delimiter=" ", fmt="%15.10f")
+print data_array
+#x=2
+#np.mean(data_array.reshape(-1, x), 1)
+
+#np.savetxt('avgdata.dat', dat, delimiter=" ", fmt="%15.10f")
