580e5750eff5d21d201cc82d4877d0a9aa20c7d8
Oliver Thomas <ot561@york.ac.uk>
2016-04-18 14:06:17 +0100

    fixed blockavg to only factorise the block length to multiples of the size of the points array does this by +1 until modulo is == 0 std dev intial is still =0 which is wrong

--- blockavg.py
+++ blockavg.py
@@ -6,7 +6,7 @@ import os
 #os.chdir(r'C:\Users\Oliver\Desktop')
 
 def blockavg(quantity, bn):
-  avg_quan = np.mean(quantity.reshape(-1, intervals), 1, dtype=np.float64)
+  avg_quan = np.mean(quantity.reshape(-1, bn), 1, dtype=np.float64)
   return avg_quan
 
 def std_deviation(array):
@@ -18,14 +18,14 @@ def std_deviation(array):
   
   return np.sqrt(meanofsq - sqofmean)
 
-#data=raw_input("enter prod tup name: ")
-data='prod_100'
+data=raw_input("enter prod tup name: ")
+#data='prod_100'
 data_tup = data + '.tup'
 
 time, t, k, pot, u, p = np.genfromtxt(data_tup, usecols=[0,1,2,3,4,5], unpack=True)
 
 #number of block lengths to try
-n_block = 100
+n_block = 1000.0
 #Allocate arrays
 b_length = np.zeros(n_block)
 std_dev = np.zeros(n_block)
@@ -37,19 +37,23 @@ std_dev = np.zeros(n_block)
 #std_dev[0] = std_deviation(u)
 
 i=0
-while (i < n_block):
-  
-  if (i>0):
-    #Calc next block length
-    b_length[i] = (i*4.0)
-  else:
+c=1
+while (c < n_block):
+  if (i==0):
     b_length[i] = 1
     intervals = 1.0
     std_dev[i] = std_deviation(u)
-  
+      
+  else:
+    #Calc next block length
+    while ((time.size % c) != 0  ):
+      c += 1
+    b_length[i] = c 
+  #c = b_length[i]
+  print b_length[i]
   #Number of blocks to average over
   intervals=(time.size/b_length[i])
-  
+  print intervals
   ##Block averaging##
   avg_u = blockavg(u, intervals)
  
@@ -57,16 +61,17 @@ while (i < n_block):
   std_dev[i]=std_deviation(avg_u)
   
   i += 1
+  c=c+1
   
   #print 'Averaged data', avg_time, avg_u
   avgdata = 'b_avg_' + data + '.dat'
   
   #write block averaged to file
   #np.savetxt(avgdata, (avg_time, avg_u), delimiter=" ", fmt="%15.10f")
-print b_length, std_dev
-plt.plot(b_length, std_dev) 
+print b_length[0:i], std_dev[0:i]
+plt.plot(b_length[0:i], std_dev[0:i]) 
 
 #plt.plot(time,u)
-#plt.savefig(data, format='png')
+plt.savefig(data, format='png')
 plt.show()
 
